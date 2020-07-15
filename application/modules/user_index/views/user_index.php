3a890bc9a29e2ceae50317d1663333af859d3d3a
Fecon <l2uncs@gmail.com>
2020-07-15 08:12:57 +0700

    update buy page

--- application/modules/user_index/views/user_index.php
+++ application/modules/user_index/views/user_index.php
@@ -1,3 +1,11 @@
+<style>
+  .digi-2 {
+    max-width: 18% !important;
+  }
+  .digi-3 {
+    max-width: 18% !important;
+  }
+</style>
 <div class="content-wrapper">
   <!-- Content Header (Page header) -->
   <section class="content-header">
@@ -19,98 +27,75 @@
   <hr>
   <!-- Main content -->
   <section class="content">
-    <?php echo form_open('lotto/rn_insert/'.$lotto[0]['id'])?>
-      <div class="row">
-        <div class="col-md-3">
-          <h4>2 ตัว</h4>
-          <?php for ($i=0; $i < 7 ; $i++) { ?>
-                    <div class="row">
-                      <div class="col-xs-10">
-                        <div class="form-group">
-                          <input type="text" maxlength="2" onkeypress="return isNumber(event)" name="2digi[]" class="form-control input-lg" placeholder="" value="">
-                        </div>
-                      </div>
-                    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <h4 class="text-center">2 ตัว</h4>
+        <div class="row">
+          <div class="col-xs-12">
+            <?php for ($i=0; $i < 5 ; $i++) { ?>
+            <div class="form-group form-2digi">
+              <input type="text" maxlength="2" onkeypress="return isNumber(event)" name="2digi[]" class="input-lg form-2digi digi-2">
+              <input type="text" maxlength="2" onkeypress="return isNumber(event)" name="2digi[]" class="input-lg form-2digi digi-2">
+              <input type="text" maxlength="2" onkeypress="return isNumber(event)" name="2digi[]" class="input-lg form-2digi digi-2">
+              <input type="text" maxlength="2" onkeypress="return isNumber(event)" name="2digi[]" class="input-lg form-2digi digi-2">
+              <input type="text" maxlength="2" onkeypress="return isNumber(event)" name="2digi[]" class="input-lg form-2digi digi-2">
+            </div>
             <?php } ?>
             <div id="add2top"></div>
 
             <div class="form-group">
               <button id="Add2" type="button" class="btn btn-default btn-lg"><i class="fa fa-plus" aria-hidden="true"></i> เพิ่มแถว</button>
             </div>
-            <div class="form-group">
-            <div class="row">
-              <div class="col-md-5">
-                  <input type="number" class="form-control input-lg" placeholder="บน">
-              </div>
-              <div class="col-md-1">
-                  <label>X</label>
-              </div>
-              <div class="col-md-5">
-                  <input type="number" class="form-control input-lg" placeholder="ล่าง">
-              </div>
-            </div>
-            </div>
-        </div>
-
-        <div class="col-md-6">
-          <div class="row">
-            <div class="col-xs-6">
-
-            </div>
-
           </div>
-          <?php for ($i=0; $i < 7 ; $i++) { ?>
-                    <div class="row">
-                      <div class="col-xs-4">
-                        <div class="form-group">
-                          <input type="text" maxlength="3" onkeypress="return isNumber(event)" name="3digi[]" class="form-control input-lg" placeholder="">
-                        </div>
-                      </div>
-                    </div>
+        </div>
+      </div>
+
+      <div class="col-md-6">
+        <h4 class="text-center">3 ตัว</h4>
+        <div class="row">
+          <div class="col-xs-12">
+            <?php for ($i=0; $i < 5 ; $i++) { ?>
+            <div class="form-group form-3digi">
+              <input type="text" maxlength="3" onkeypress="return isNumber(event)" name="3digi[]" class="input-lg form-3digi digi-3">
+              <input type="text" maxlength="3" onkeypress="return isNumber(event)" name="3digi[]" class="input-lg form-3digi digi-3">
+              <input type="text" maxlength="3" onkeypress="return isNumber(event)" name="3digi[]" class="input-lg form-3digi digi-3">
+              <input type="text" maxlength="3" onkeypress="return isNumber(event)" name="3digi[]" class="input-lg form-3digi digi-3">
+              <input type="text" maxlength="3" onkeypress="return isNumber(event)" name="3digi[]" class="input-lg form-3digi digi-3">
+            </div>
             <?php } ?>
             <div id="add3top"></div>
             <div class="form-group">
                 <button id="Add3" type="button" class="btn btn-default btn-lg"><i class="fa fa-plus" aria-hidden="true"></i> เพิ่มแถว</button>
             </div>
+          </div>
         </div>
       </div>
-      <div class="row text-center">
-        <button type="submit" id="Add2" class="btn btn-success btn-lg">บันทึก</button>
-      </div>
-    <?php echo form_close(); ?>
+    </div>
   </section>
   <!-- /.content -->
   </div>
 
   <script>
   $(document).ready(function() {
+
     $("#Add2").on("click", function() {
-        $("#add2top").append("<div class='row'>"
-        + "<div class='col-xs-4'>"
-        + "<div class='form-group'>"
-        +	"<input type='text' maxlength='2' onkeypress='return isNumber(event)' name='2digi[]' class='form-control input-lg'>"
-        +	"</div>"
-        +	"</div>"
+        $("#add2top").append("<div class='form-group form-2digi'>"
+        +	"<input type='text' maxlength='2' onkeypress='return isNumber(event)' name='2digi[]' class='input-lg form-2digi digi-2'>"
+        + "<input type='text' maxlength='2' onkeypress='return isNumber(event)' name='2digi[]' class='input-lg form-2digi digi-2'>"
+        + "<input type='text' maxlength='2' onkeypress='return isNumber(event)' name='2digi[]' class='input-lg form-2digi digi-2'>"
+        + "<input type='text' maxlength='2' onkeypress='return isNumber(event)' name='2digi[]' class='input-lg form-2digi digi-2'>"
+        + "<input type='text' maxlength='2' onkeypress='return isNumber(event)' name='2digi[]' class='input-lg form-2digi digi-2'>"
         +	"</div>");
     });
+
     $("#Add3").on("click", function() {
-        $("#add3top").append("<div class='row'>"
-        + "<div class='col-xs-4'>"
-        + "<div class='form-group'>"
-        +	"<input type='text' maxlength='3' onkeypress='return isNumber(event)' name='3digi[]' class='form-control input-lg' placeholder=''>"
-        +	"</div>"
-        +	"</div>"
-        + "<div class='col-xs-4'>"
-        +	"<div class='form-group'>"
-        +	"<input type='number' name='3pay[]' class='form-control input-lg' placeholder=''>"
-        +	"</div>"
-        +	"</div>"
-        + "<div class='col-xs-4'>"
-        +	"<div class='form-group'>"
-        +	"<input type='number' name='3pay2[]' class='form-control input-lg' placeholder=''>"
-        +	"</div>"
-        +	"</div>"
-        +	"</div>");
+      $("#add3top").append("<div class='form-group form-3digi'>"
+        + "<input type='text' maxlength='2' onkeypress='return isNumber(event)' name='2digi[]' class='input-lg form-3digi digi-3'>"
+        + "<input type='text' maxlength='2' onkeypress='return isNumber(event)' name='2digi[]' class='input-lg form-3digi digi-3'>"
+        + "<input type='text' maxlength='2' onkeypress='return isNumber(event)' name='2digi[]' class='input-lg form-3digi digi-3'>"
+        + "<input type='text' maxlength='2' onkeypress='return isNumber(event)' name='2digi[]' class='input-lg form-3digi digi-3'>"
+        + "<input type='text' maxlength='2' onkeypress='return isNumber(event)' name='2digi[]' class='input-lg form-3digi digi-3'>"
+        + "</div>");
     });
 
     $("#Remove").on("click", function() {
@@ -118,5 +103,40 @@
     });
   });
 
+  $(".form-2digi.digi-2").keyup(function () {
+   if (this.value.length == this.maxLength) {
+     $(this)
+      .blur()
+      .parent()
+      .next()
+      .children('.form-2digi.digi-2:enabled:first')
+      .focus();
+   }
+  });
+
+  $(".digi-2").keyup(function () {
+   if (this.value.length == this.maxLength) {
+     $(this)
+      .next('.digi-2')
+      .focus();
+   }
+  });
+
+  $(".form-3digi.digi-3").keyup(function () {
+   if (this.value.length == this.maxLength) {
+     $(this)
+      .blur()
+      .parent()
+      .next()
+      .children('.form-3digi.digi-3:enabled:first')
+      .focus();
+   }
+  });
+
+  $(".digi-3").keyup(function () {
+      if (this.value.length == this.maxLength) {
+        $(this).next('.digi-3').focus();
+      }
+  });
 
   </script>
