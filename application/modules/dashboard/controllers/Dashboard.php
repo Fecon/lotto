393a96aed7b1fb91e79ea11c9c50039be7a44862
Sarun.S <l2uncs@gmail.com>
2020-11-30 08:14:44 +0700

    Report realtime (WIP)

--- application/modules/dashboard/controllers/Dashboard.php
+++ application/modules/dashboard/controllers/Dashboard.php
@@ -148,7 +148,7 @@ class Dashboard extends MX_Controller {
 			$data['percent_2top'] 		 = $this->get_percent_type($lotto_id,$agentInfo,2,'top');
 			$data['percent_2bottom'] 	 = $this->get_percent_type($lotto_id,$agentInfo,2,'bottom');
 			$data['percent_3top'] 		 = $this->get_percent_type($lotto_id,$agentInfo,3,'top');
-			$data['percent_2tod'] 		 = $this->get_percent_type($lotto_id,$agentInfo,3,'bottom');
+			$data['percent_3tod'] 		 = $this->get_percent_type($lotto_id,$agentInfo,3,'bottom');
 
 			$data['number_2top'] 		 = $this->Dashboard_model->get_agent_buy_number($lotto_id,2,'top',$config[5]['value'],$agent_id);
 			$data['number_2bottom'] 	 = $this->Dashboard_model->get_agent_buy_number($lotto_id,2,'bottom',$config[5]['value'],$agent_id);
@@ -167,7 +167,7 @@ class Dashboard extends MX_Controller {
 			$data['percent_2top'] 		 = $this->get_percent_type($lotto_id,$data['list_agent'],2,'top');
 			$data['percent_2bottom'] 	 = $this->get_percent_type($lotto_id,$data['list_agent'],2,'bottom');
 			$data['percent_3top'] 		 = $this->get_percent_type($lotto_id,$data['list_agent'],3,'top');
-			$data['percent_2tod'] 		 = $this->get_percent_type($lotto_id,$data['list_agent'],3,'bottom');
+			$data['percent_3tod'] 		 = $this->get_percent_type($lotto_id,$data['list_agent'],3,'bottom');
 
 			$data['number_2top'] 		 = $this->Dashboard_model->get_total_buy_number($lotto_id,2,'top',$config[5]['value']);
 			$data['number_2bottom'] 	 = $this->Dashboard_model->get_total_buy_number($lotto_id,2,'bottom',$config[5]['value']);
@@ -191,6 +191,7 @@ class Dashboard extends MX_Controller {
 	public function report_api()
 	{
 
+		$list_agent = $this->Dashboard_model->list_agent();
 		$config = $this->Dashboard_model->get_config();
 
 		if(empty($this->input->post())){
@@ -203,7 +204,6 @@ class Dashboard extends MX_Controller {
 		}
 
 		if($agent_id!=0){
-			$data['agentInfo']	= $this->Dashboard_model->get_agent($agent_id);
 			$agentInfo			= $this->Dashboard_model->get_agents($agent_id);
 
 			$data['agent_sent'] = $this->Dashboard_model->get_sum_agent_received($lotto_id,$agent_id);
@@ -212,7 +212,7 @@ class Dashboard extends MX_Controller {
 			$data['percent_2top'] 		 = $this->get_percent_type($lotto_id,$agentInfo,2,'top');
 			$data['percent_2bottom'] 	 = $this->get_percent_type($lotto_id,$agentInfo,2,'bottom');
 			$data['percent_3top'] 		 = $this->get_percent_type($lotto_id,$agentInfo,3,'top');
-			$data['percent_2tod'] 		 = $this->get_percent_type($lotto_id,$agentInfo,3,'bottom');
+			$data['percent_3tod'] 		 = $this->get_percent_type($lotto_id,$agentInfo,3,'bottom');
 
 			$data['number_2top'] 		 = $this->Dashboard_model->get_agent_buy_number($lotto_id,2,'top',$config[5]['value'],$agent_id);
 			$data['number_2bottom'] 	 = $this->Dashboard_model->get_agent_buy_number($lotto_id,2,'bottom',$config[5]['value'],$agent_id);
@@ -226,12 +226,12 @@ class Dashboard extends MX_Controller {
 
 		}else{
 			
-			$data['percent_total'] 		 = $this->get_percent($lotto_id,$data['list_agent']);
+			$data['percent_total'] 		 = $this->get_percent($lotto_id,$list_agent);
 
-			$data['percent_2top'] 		 = $this->get_percent_type($lotto_id,$data['list_agent'],2,'top');
-			$data['percent_2bottom'] 	 = $this->get_percent_type($lotto_id,$data['list_agent'],2,'bottom');
-			$data['percent_3top'] 		 = $this->get_percent_type($lotto_id,$data['list_agent'],3,'top');
-			$data['percent_2tod'] 		 = $this->get_percent_type($lotto_id,$data['list_agent'],3,'bottom');
+			$data['percent_2top'] 		 = $this->get_percent_type($lotto_id,$list_agent,2,'top');
+			$data['percent_2bottom'] 	 = $this->get_percent_type($lotto_id,$list_agent,2,'bottom');
+			$data['percent_3top'] 		 = $this->get_percent_type($lotto_id,$list_agent,3,'top');
+			$data['percent_3tod'] 		 = $this->get_percent_type($lotto_id,$list_agent,3,'bottom');
 
 			$data['number_2top'] 		 = $this->Dashboard_model->get_total_buy_number($lotto_id,2,'top',$config[5]['value']);
 			$data['number_2bottom'] 	 = $this->Dashboard_model->get_total_buy_number($lotto_id,2,'bottom',$config[5]['value']);
@@ -243,8 +243,6 @@ class Dashboard extends MX_Controller {
 			$data['agent_sent']['2digi'] = $this->Dashboard_model->get_sum_received($lotto_id,2);
 			$data['agent_sent']['3digi'] = $this->Dashboard_model->get_sum_received($lotto_id,3);
 		}
-
-		$data['lotto_id']	= $lotto_id;
 
 		// echo "<pre>";
 		// print_r($data);
--- application/modules/dashboard/views/report.php
+++ application/modules/dashboard/views/report.php
@@ -35,7 +35,7 @@
           <div class="col-md-3">
             <div class="form-group">
               <label>ตัวแทน</label>
-              <select name="agent_id" class="form-control select2" style="width: 100%;" onchange="this.form.submit()">
+              <select name="agent_id" id="agent_id" class="form-control select2" style="width: 100%;" onchange="this.form.submit()">
                 <option value="0">ทั้งหมด</option>
                 <?php foreach ($list_agent as $key => $agent) { ?>
                 <option value="<?php echo $agent['id']; ?>" <?php
@@ -52,7 +52,7 @@
           <div class="col-md-3">
             <div class="form-group">
               <label>งวดประจำวันที่</label>
-              <select name="lotto_id" class="form-control select2" style="width: 100%;">
+              <select name="lotto_id" id="lotto_id" class="form-control select2" style="width: 100%;">
                 <?php foreach ($list_lotto as $key => $lotto) { 
                               $date = new DateTime($lotto['name']);
                                 $date_display = $date->format('d/m/Y');
@@ -76,15 +76,16 @@
             // Set Variable //
               $sent_2digi         = $agent_sent['2digi']['top'] + $agent_sent['2digi']['bottom'];
               $sent_2digi_display = number_format($sent_2digi);
-              $sent_3top         = $agent_sent['3digi']['top'];
-              $sent_3top_display = number_format($sent_3top);
-              $sent_3tod         = $agent_sent['3digi']['bottom'];
-              $sent_3tod_display = number_format($sent_3tod);
-              $total_sent       = $sent_2digi+$sent_3top+$sent_3tod;
+              $sent_3top          = $agent_sent['3digi']['top'];
+              $sent_3top_display  = number_format($sent_3top);
+              $sent_3tod          = $agent_sent['3digi']['bottom'];
+              $sent_3tod_display  = number_format($sent_3tod);
+              $total_sent         = $sent_2digi+$sent_3top+$sent_3tod;
         ?>
 
         <div class="box box-info">
           <div class="box-body">
+
             <br>
             <div class="row">
               <div class="col-md-12">
@@ -99,13 +100,13 @@
               </div>
               <div class="col-md-4">
                 <label>ยอดรวมทั้งหมดที่ส่งมา</label>
-                <span><?php 
+                <span id="total_sent"><?php 
                         echo number_format($total_sent);
                       ?></span>
               </div>
               <div class="col-md-3">
                 <label>ยอดรวมหัก % :</label>
-                <span><?php echo number_format($total_sent - $percent_total); ?></span>
+                <span id="total_net"><?php echo number_format($total_sent - $percent_total); ?></span>
               </div>
             </div>
             <br>
@@ -118,21 +119,25 @@
                   <tr>
                     <th class="text-center">ยอดรวม</th>
                     <th colspan="2" class="text-center">
-                      <?php 
-                        echo number_format($agent_sent['2digi']['top']);
-                      ?>
+                      <span id="2digi_top">
+                        <?php 
+                          echo number_format($agent_sent['2digi']['top']);
+                        ?>
+                      </span> 
                     </th>
                   </tr>
                   <tr>
                     <th class="text-center">ยอดรวมหัก %</th>
                     <th colspan="2" class="text-center">
-                      <?php 
-                        echo number_format($agent_sent['2digi']['top'] - $percent_2top);
-                      ?>
+                      <span id="2top_net">
+                        <?php 
+                          echo number_format($agent_sent['2digi']['top'] - $percent_2top);
+                        ?>
+                      </span> 
                     </th>
                   </tr>
                 </table>
-                <table class="table table-striped buyTable">
+                <table class="table table-striped buyTable" id="2digi_topTable">
                   <thead>
                     <tr>
                       <th class="text-center text-small">เลข</th>
@@ -140,7 +145,7 @@
                       <th class="text-center text-small">เกินมา</th>
                     </tr>
                   </thead>
-                  <tbody>
+                  <tbody id="2digi_top_tr">
                     <?php foreach ($number_2top as $key => $value) { 
                             if($value['sent'] >= $config[3]['value']) {
                               $bgClass = 'bg-red';
@@ -185,17 +190,22 @@
                   <tr>
                     <th class="text-center">ยอดรวม</th>
                     <th colspan="2" class="text-center">
-                      <?php 
-                        echo number_format($agent_sent['2digi']['bottom']);
-                      ?>
+                      <span id="2digi_bottom">
+                        <?php 
+                          echo number_format($agent_sent['2digi']['bottom']);
+                        ?>
+                      </span> 
                     </th>
                   </tr>
                   <tr>
                     <th class="text-center">ยอดรวมหัก %</th>
                     <th colspan="2" class="text-center">
-                      <?php 
-                        echo number_format($agent_sent['2digi']['bottom'] - $percent_2bottom);
-                      ?>
+                      <span id="2bottom_net">
+                        <?php 
+                          echo number_format($agent_sent['2digi']['bottom'] - $percent_2bottom);
+                        ?>
+                      </span>
+                      
                     </th>
                   </tr>
                 </table>
@@ -228,7 +238,7 @@
                         <span><?php echo $value['number'] ?></span>
                       </td>
                       <td class="text-center <?php echo $bgClass ; ?>">
-                        <span><?php echo number_format($value['sent']) ?></span>
+                        <span id=""><?php echo number_format($value['sent']) ?></span>
                       </td>
                       <td class="text-center <?php echo $bgClass2 ; ?>">
                         <span>
@@ -252,17 +262,21 @@
                   <tr>
                     <th class="text-center">ยอดรวม</th>
                     <th colspan="2" class="text-center">
-                      <?php 
-                        echo number_format($agent_sent['3digi']['top']);
-                      ?>
+                      <span id="3digi_top">
+                        <?php 
+                          echo number_format($agent_sent['3digi']['top']);
+                        ?>
+                      </span>
                     </th>
                   </tr>
                   <tr>
                     <th class="text-center">ยอดรวมหัก %</th>
                     <th colspan="2" class="text-center">
-                      <?php 
-                        echo number_format($agent_sent['3digi']['top'] - $percent_3top);
-                      ?>
+                      <span id="3top_net">
+                        <?php 
+                          echo number_format($agent_sent['3digi']['top'] - $percent_3top);
+                        ?>
+                      </span>
                     </th>
                   </tr>
                 </table>
@@ -320,17 +334,21 @@
                   <tr>
                     <th class="text-center">ยอดรวม</th>
                     <th colspan="2" class="text-center">
-                      <?php 
-                        echo number_format($agent_sent['3digi']['bottom']);
-                      ?>
+                      <span id="3digi_tod">
+                        <?php 
+                          echo number_format($agent_sent['3digi']['bottom']);
+                        ?>
+                      </span>
                     </th>
                   </tr>
                   <tr>
                     <th class="text-center">ยอดรวมหัก %</th>
                     <th colspan="2" class="text-center">
-                      <?php 
-                        echo number_format($agent_sent['3digi']['bottom'] - $percent_2tod);
-                      ?>
+                      <span id="3tod_net">
+                        <?php 
+                          echo number_format($agent_sent['3digi']['bottom'] - $percent_3tod);
+                        ?>
+                      </span>
                     </th>
                   </tr>
                 </table>
@@ -397,31 +415,97 @@
         [1, "desc"]
       ]
     });
-  
 
-   $('#report').change(function(){
-    var username = $(this).val();
-    $.ajax({
-       url:'<?php echo site_url('dashboard/report_api');?>',
-       method: 'post',
-       data: {username: username},
-       dataType: 'json',
-       success: function(response){
-         var len = response.length;
-         $('#suname,#sname,#semail').text('');
-         if(len > 0){
-           // Read values
-           var uname = response[0].username;
-           var name = response[0].name;
-           var email = response[0].email;
-   
-           $('#suname').text(uname);
-           $('#sname').text(name);
-           $('#semail').text(email);
+    function formatNumber(num) {
+      return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+    }
+
+    // load_data();
+    setInterval(function(){load_data(); }, 10000);
+
+    function load_data(query)
+     {
+        $.ajax({
+         url:'<?php echo site_url('dashboard/report_api');?>',
+         method:"POST",
+         data:{agent_id:$("#agent_id").val() , lotto_id:$("#lotto_id").val() , query:query},
+         success:function(data){
+
+          var result = JSON.parse(data);
+
+          var sent_2digi          = parseInt(result.agent_sent['2digi']['top']) + parseInt(result.agent_sent['2digi']['bottom']);
+          var sent_3top           = parseInt(result.agent_sent['3digi']['top']);
+          var sent_3tod           = parseInt(result.agent_sent['3digi']['bottom']);
+          var total_sent          = sent_2digi + sent_3top + sent_3tod;
+          var total_net           = total_sent - result.percent_total;
+
+          var sent_2top_net       = parseInt(result.agent_sent['2digi']['top']) - parseInt(result.percent_2top);
+          var sent_2bottom_net    = parseInt(result.agent_sent['2digi']['bottom']) - parseInt(result.percent_2bottom);
+          var sent_3top_net       = parseInt(result.agent_sent['3digi']['top']) - parseInt(result.percent_3top);
+          var sent_3tod_net       = parseInt(result.agent_sent['3digi']['bottom']) - parseInt(result.percent_3tod);
+
+          
+
+          document.getElementById('total_sent').innerHTML     = formatNumber(total_sent);
+          document.getElementById('total_net').innerHTML      = formatNumber(total_net);
+
+          document.getElementById('2digi_top').innerHTML      = formatNumber(result.agent_sent['2digi']['top']);
+          document.getElementById('2digi_bottom').innerHTML   = formatNumber(result.agent_sent['2digi']['bottom']);
+          document.getElementById('3digi_top').innerHTML      = formatNumber(result.agent_sent['3digi']['top']);
+          document.getElementById('3digi_tod').innerHTML      = formatNumber(result.agent_sent['3digi']['bottom']);
+
+          document.getElementById('2top_net').innerHTML       = formatNumber(sent_2top_net);
+          document.getElementById('2bottom_net').innerHTML    = formatNumber(sent_2bottom_net);
+          document.getElementById('3top_net').innerHTML       = formatNumber(sent_3top_net);
+          document.getElementById('3tod_net').innerHTML       = formatNumber(sent_3tod_net);
+          // $('#result').html(data);
+          // console.log(result.agent_sent['2digi']);
+          var config_3 = <?php echo $config[3]['value'] ?> ;
+          var config_4 = <?php echo $config[4]['value'] ?> ;
+          var config_5 = <?php echo $config[5]['value'] ?> ;
+          var config_6 = <?php echo $config[6]['value'] ?> ;
+          var config_7 = <?php echo $config[7]['value'] ?> ;
+          var config_8 = <?php echo $config[8]['value'] ?> ;
+          var config_9 = <?php echo $config[9]['value'] ?> ;
+          var config_10 = <?php echo $config[10]['value'] ?> ;
+          var config_11 = <?php echo $config[11]['value'] ?> ;
+
+          console.log(result.number_2top);
+
+
+
+          if(result.number_2top!= '') {
+
+              //$("#myTable tbody tr:not(:first-child)").remove();
+              $("#2digi_top_tr").empty();
+              $.each(result.number_2top, function(key, val) {
+
+                  let bgClass = '' ;
+                  let bgClass2 = '' ;
+
+                  if(val['sent'] >= config_3) {
+                    bgClass = 'bg-red';
+                    bgClass2 = 'bg-red';
+                  }else if(val['sent'] > config_4) {
+                    bgClass = 'bg-yellow';
+                    bgClass2 = '';
+                  }else if(val['sent'] > config_5) {
+                    bgClass = 'bg-green';
+                    bgClass2 = '';
+                  }
+
+                  var tr = "<tr>";
+                  tr = tr + "<td class='text-center'>" + val["number"] + "</td>";
+                  tr = tr + "<td class='text-center "+ bgClass +"'>" + formatNumber(val["sent"]) + "</td>";
+                  tr = tr + "<td class='text-center "+ bgClass2 +"'>" + formatNumber(parseInt(val["sent"]-config_3)) + "</td>";
+                  tr = tr + "</tr>";
+                  $('#2digi_topTable > tbody:last').append(tr);
+              });
+          }
+          
          }
-       }
-   });
-  });
-
- });
+      })
+     }
+});
 </script>
+
